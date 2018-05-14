c9b0a4fa2e6ca3b5b2c6aa525925ed532a128226
Aristamo <AljoschaBurgmaier@yahoo.de>
2018-05-14 18:18:37 +0200

    Added a simple Player Controller

---
+++ Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerControlls : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    public float FallMutiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(h * Speed, 0, 0) * Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1") && isGrounded)
+        {
+            rb.velocity += Vector3.up * JumpForce;
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (FallMutiplier - 1) * Time.fixedDeltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Fire1"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
