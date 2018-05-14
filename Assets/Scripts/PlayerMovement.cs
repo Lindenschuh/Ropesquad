cef81649ce2eeac875d2be40da90c67125a3ad73
Lars Assmus <lassmus@stud.hs-heilbronn.de>
2018-05-14 14:20:38 +0200

    init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform Player;
+
+    private bool _isJumping;
+
+    private Vector3 peak;
+    private Vector3 destination;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, -Input.GetAxis("Horizontal"), 0));
+
+        transform.position = new Vector3(transform.position.x, Player.transform.position.y, transform.position.z);
+
+        if (!_isJumping)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _isJumping = true;
+            }
+        }
+        else
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/_2DProjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _2DProjection : MonoBehaviour
+{
+    // Use this for initialization
+    public Camera cam;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        movement = Camera.main.transform.TransformDirection(movement);
+        transform.Translate(movement);
+    }
+}
