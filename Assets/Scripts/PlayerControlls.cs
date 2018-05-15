d84e516753afb2348fb34d86c0f582531bec616a
Aristamo <AljoschaBurgmaier@yahoo.de>
2018-05-15 13:40:59 +0200

    Controlls around are working now and deleted old scenes

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Joysticks;
+using System;
 
 public class PlayerControlls : MonoBehaviour
 {
@@ -10,7 +11,6 @@ public class PlayerControlls : MonoBehaviour
 
     public Transform tower;
 
-    [Range(0f, 1f)]
     public float Speed;
 
     private float RotateSpeed = 1f;
@@ -18,7 +18,12 @@ public class PlayerControlls : MonoBehaviour
     public float CameraOffset = 10;
     public byte PlayerNumber;
 
+    public RopeController Rope;
+
+    public float StartAngle = 5;
+
     private Vector2 _centre;
+    private Vector2 _camCentre;
     private float _angle;
     private float _timer;
     private float _radius;
@@ -29,14 +34,31 @@ public class PlayerControlls : MonoBehaviour
 
     public bool InCameraFocus;
 
+    public float LookDir = 45f;
+
     private bool isGrounded;
     private Rigidbody rb;
     private JoystickManager _joyManager;
 
     private void Start()
     {
-        _centre = transform.position;
         _radius = tower.localScale.x / 2 + CharacterOffset;
+
+        var x = _radius * Mathf.Sin(StartAngle);
+        var z = _radius * Mathf.Cos(StartAngle);
+
+        transform.position = new Vector3(x, transform.position.y, z);
+
+        if (InCameraFocus)
+        {
+            x = (_radius + CameraOffset) * Mathf.Sin(StartAngle);
+            z = (_radius + CameraOffset) * Mathf.Cos(StartAngle);
+
+            cam.transform.position = new Vector3(x, transform.position.y, z);
+            _camCentre = new Vector2(transform.position.x, transform.position.z);
+        }
+
+        _centre = new Vector2(transform.position.x, transform.position.z);
         rb = GetComponent<Rigidbody>();
         _joyManager = new JoystickManager(PlayerNumber);
     }
@@ -44,13 +66,75 @@ public class PlayerControlls : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        cam.transform.LookAt(new Vector3(tower.position.x, transform.position.y, tower.position.z));
-        _timer += -_joyManager.GetAxis(JoystickAxis.HORIZONTAL) * Time.deltaTime * Speed;
-        var angle = _timer;
-        this.transform.position = new Vector3((_centre.x + Mathf.Sin(angle) * _radius), transform.position.y, ((_centre.y + Mathf.Cos(angle) * _radius)));
-        if (InCameraFocus)
-            cam.transform.position = new Vector3((_centre.x + Mathf.Sin(angle) * (_radius + CameraOffset)), transform.position.y, ((_centre.y + Mathf.Cos(angle) * (_radius + CameraOffset))));
+        Movement();
+        Jumping();
+        RopeInteraction();
+        CheckPosition();
+    }
 
+    private void RopeInteraction()
+    {
+        // Einziehen
+        if (_joyManager.CheckButton(JoystickButton.BUMPER_L, Input.GetButton) && Rope.RopeLength > Rope.MinRopeLength)
+        {
+            Rope.UpdateWinch(Rope.RopeLength - Rope.WinchSpeed * Time.deltaTime);
+        }
+        // Seil lassen;
+        if (_joyManager.CheckButton(JoystickButton.BUMPER_L, Input.GetButton) && Rope.RopeLength < Rope.MaxRopeLength)
+        {
+            Rope.UpdateWinch(Rope.RopeLength + Rope.WinchSpeed * Time.deltaTime);
+        }
+    }
+
+    private void Movement()
+    {
+        var h = _joyManager.GetAxis(JoystickAxis.HORIZONTAL);
+
+        transform.RotateAround(tower.position, Vector3.up, h * Speed * Time.deltaTime);
+
+        var desiredPos = (transform.position - tower.position).normalized * _radius + tower.position;
+
+        if (Rope.CheckNewPosition(desiredPos, PlayerNumber))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, desiredPos, Time.deltaTime * Speed);
+        }
+
+        // Coorect Look Dir
+        var lookDir = (h > 0) ? 0 : 180;
+        Vector3 tangentVector = Quaternion.Euler(0, lookDir, 0) * (transform.position - new Vector3(tower.position.x, transform.position.y, tower.position.z));
+        transform.rotation = Quaternion.LookRotation(tangentVector);
+
+        // Check if inside Radius
+        var towerCenter = new Vector3(tower.position.x, transform.position.y, tower.position.z);
+        var distanceSqr = (transform.position - towerCenter).sqrMagnitude;
+        float radiusSqr = _radius * _radius;
+
+        if (distanceSqr != radiusSqr)
+        {
+            transform.position = towerCenter + (transform.position - towerCenter).normalized * _radius;
+        }
+
+        //var tmpTimer = _timer - _joyManager.GetAxis(JoystickAxis.HORIZONTAL) * Time.deltaTime * Speed;
+        //var angle = tmpTimer;
+        //var newPos = new Vector3((_centre.x + Mathf.Sin(angle) * _radius), transform.position.y, ((_centre.y + Mathf.Cos(angle) * _radius)));
+        ////if (Rope.CheckNewPosition(newPos, transform))
+        ////{
+        //_timer = tmpTimer;
+        //transform.position = newPos;
+
+        //if (InCameraFocus)
+        //{
+        //    cam.transform.LookAt(new Vector3(tower.position.x, transform.position.y, tower.position.z));
+        //    cam.transform.position = new Vector3((_camCentre.x + Mathf.Sin(angle) * (_radius + CameraOffset)), transform.position.y, ((_camCentre.y + Mathf.Cos(angle) * (_radius + CameraOffset))));
+        //}
+    }
+
+    private void CheckPosition()
+    {
+    }
+
+    private void Jumping()
+    {
         if (_joyManager.CheckButton(JoystickButton.A, Input.GetButtonDown) && isGrounded)
         {
             rb.velocity += Vector3.up * JumpForce;
---
+++ Assets/Scripts/RopeController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+    public Transform ConnectionOne;
+    public Transform ConnectionTwo;
+
+    private LineRenderer lineRenderer;
+
+    public List<Vector3> RopeSections;
+
+    [Range(1f, 20f)]
+    public float RopeLength = 1f;
+
+    public float MinRopeLength = 1f;
+    public float MaxRopeLength = 20f;
+
+    [Range(0f, 0.05f)]
+    public float stretch = 0.01f;
+
+    public float RopeWidth = .2f;
+
+    public float WinchSpeed = .2f;
+
+    private float loadMass = 100f;
+
+    private SpringJoint springJoint;
+
+    private void Start()
+    {
+        springJoint = ConnectionOne.GetComponent<SpringJoint>();
+
+        lineRenderer = GetComponent<LineRenderer>();
+
+        UpdateSpring();
+    }
+
+    private void Update()
+    {
+        DisplayRope();
+    }
+
+    private void UpdateSpring()
+    {
+        float density = 7750f;
+
+        float radius = 0.02f;
+
+        float volume = Mathf.PI * radius * radius * RopeLength;
+
+        float ropeMass = volume * density;
+
+        ropeMass += loadMass;
+
+        float ropeForce = ropeMass * Physics.gravity.y;
+
+        float kRope = ropeForce / stretch;
+
+        springJoint.spring = kRope * 1f;
+        springJoint.damper = kRope * 0.8f;
+
+        springJoint.maxDistance = RopeLength;
+    }
+
+    public void UpdateWinch(float newLength)
+    {
+        RopeLength = Mathf.Clamp(newLength, MinRopeLength, MaxRopeLength);
+
+        UpdateSpring();
+    }
+
+    public bool IsOnMaxLength()
+    {
+        return (ConnectionOne.position - ConnectionTwo.position).magnitude >= RopeLength;
+    }
+
+    public bool CheckNewPosition(Vector3 newPos, byte player)
+    {
+        if (player == 1)
+        {
+            return (newPos - ConnectionTwo.position).magnitude < RopeLength;
+        }
+        else
+        {
+            return (newPos - ConnectionOne.position).magnitude < RopeLength;
+        }
+    }
+
+    private void DisplayRope()
+    {
+        lineRenderer.startWidth = RopeWidth;
+        lineRenderer.endWidth = RopeWidth;
+
+        Vector3 A = ConnectionOne.position;
+        Vector3 D = ConnectionTwo.position;
+
+        Vector3 B = A + Vector3.zero * .5f * (-(A - D).magnitude * 0.1f);
+
+        Vector3 C = D + Vector3.zero * .5f * ((A - D).magnitude * .5f);
+
+        BezierCurve.GetBezierCurve(A, B, C, D, out RopeSections);
+
+        lineRenderer.positionCount = RopeSections.Count;
+
+        lineRenderer.SetPositions(RopeSections.ToArray());
+    }
+}
