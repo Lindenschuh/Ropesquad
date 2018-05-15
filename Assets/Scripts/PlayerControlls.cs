f28650e16e2f047f4c4bf8d20bc377f5b69320fa
Aristamo <AljoschaBurgmaier@yahoo.de>
2018-05-15 09:17:03 +0200

    Merged Player Controlls

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -1,32 +1,57 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using Joysticks;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerControlls : MonoBehaviour
 {
+    // Use this for initialization
+    public Camera cam;
+
+    public Transform tower;
+
+    [Range(0f, 1f)]
     public float Speed;
+
+    private float RotateSpeed = 1f;
+    public float CharacterOffset = 5f;
+    public float CameraOffset = 10;
+    public byte PlayerNumber;
+
+    private Vector2 _centre;
+    private float _angle;
+    private float _timer;
+    private float _radius;
+
     public float JumpForce;
     public float FallMutiplier = 2.5f;
     public float LowJumpMultiplier = 2f;
-    public byte PlayerNumber;
+
+    public bool InCameraFocus;
 
     private bool isGrounded;
     private Rigidbody rb;
-    private JoystickManager joyManager;
+    private JoystickManager _joyManager;
 
     private void Start()
     {
+        _centre = transform.position;
+        _radius = tower.localScale.x / 2 + CharacterOffset;
         rb = GetComponent<Rigidbody>();
-        joyManager = new JoystickManager(PlayerNumber);
+        _joyManager = new JoystickManager(PlayerNumber);
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    private void Update()
     {
-        var h = joyManager.GetAxis(JoystickAxis.HORIZONTAL);
+        cam.transform.LookAt(new Vector3(tower.position.x, transform.position.y, tower.position.z));
+        _timer += -_joyManager.GetAxis(JoystickAxis.HORIZONTAL) * Time.deltaTime * Speed;
+        var angle = _timer;
+        this.transform.position = new Vector3((_centre.x + Mathf.Sin(angle) * _radius), transform.position.y, ((_centre.y + Mathf.Cos(angle) * _radius)));
+        if (InCameraFocus)
+            cam.transform.position = new Vector3((_centre.x + Mathf.Sin(angle) * (_radius + CameraOffset)), transform.position.y, ((_centre.y + Mathf.Cos(angle) * (_radius + CameraOffset))));
 
-        transform.position += new Vector3(h * Speed, 0, 0) * Time.deltaTime;
-
-        if (joyManager.CheckButton(JoystickButton.A, Input.GetButtonDown) && isGrounded)
+        if (_joyManager.CheckButton(JoystickButton.A, Input.GetButtonDown) && isGrounded)
         {
             rb.velocity += Vector3.up * JumpForce;
         }
@@ -35,7 +60,7 @@ public class PlayerControlls : MonoBehaviour
         {
             rb.velocity += Vector3.up * Physics.gravity.y * (FallMutiplier - 1) * Time.fixedDeltaTime;
         }
-        else if (rb.velocity.y > 0 && !joyManager.CheckButton(JoystickButton.A, Input.GetButton))
+        else if (rb.velocity.y > 0 && !_joyManager.CheckButton(JoystickButton.A, Input.GetButton))
         {
             rb.velocity += Vector3.up * Physics.gravity.y * (LowJumpMultiplier - 1) * Time.fixedDeltaTime;
         }
