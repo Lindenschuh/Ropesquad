f7e3e21c4f47effd1828a6384c5501e4287ad3c0
Aristamo <AljoschaBurgmaier@yahoo.de>
2018-05-16 08:40:00 +0200

    Added a Demo Scene and a Movment Script for Plattforms

---
+++ Assets/PlattformMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Movement
+{
+    public enum MoveDir
+    {
+        HORIZONTAL,
+        VERTICAL,
+        NONE
+    }
+
+    public class PlattformMove : MonoBehaviour
+    {
+        [Range(.2f, 2f)]
+        public float MoveDistance = 1;
+
+        public Tower Tower;
+
+        public float MovementSpeed;
+
+        public MoveDir Direction;
+
+        private float lookDir;
+
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            switch (Direction)
+            {
+                case MoveDir.HORIZONTAL:
+                    MovementManger.NextPlattformPosition(transform, Mathf.Sin(Time.time / MoveDistance), MovementSpeed, Tower, Tower.CharacterLayer);
+                    break;
+
+                case MoveDir.VERTICAL:
+                    transform.Translate(Vector3.up * Mathf.Sin(Time.time / MoveDistance) * MovementSpeed * Time.fixedDeltaTime);
+                    MovementManger.SnapToGrid(transform, Tower, Tower.CharacterLayer);
+                    break;
+
+                case MoveDir.NONE:
+                    MovementManger.SnapToGrid(transform, Tower, Tower.CharacterLayer);
+                    break;
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.layer == 9)
+            {
+                other.transform.parent = gameObject.transform;
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.layer == 9)
+            {
+                other.transform.parent = null;
+            }
+        }
+    }
+}
--- Assets/Scripts/MovementManger.cs
+++ Assets/Scripts/MovementManger.cs
@@ -27,4 +27,40 @@ public static class MovementManger
             target.position = towerCenter + (target.position - towerCenter).normalized * playerRadius;
         }
     }
+
+    public static void NextPlattformPosition(Transform target, float hInput, float speed, Tower center, float movementlayer)
+    {
+        var towerCenter = new Vector3(center.transform.position.x, target.position.y, center.transform.position.z);
+        var playerRadius = center.Radius + movementlayer;
+
+        target.RotateAround(center.transform.position, Vector3.up, hInput * speed * Time.fixedDeltaTime);
+
+        Vector3 tangentVector = Quaternion.Euler(0, 0, 0) * (target.position - towerCenter);
+        target.rotation = Quaternion.LookRotation(tangentVector);
+
+        var distanceSqr = (target.position - towerCenter).sqrMagnitude;
+        float radiusSqr = center.Radius * center.Radius;
+
+        if (distanceSqr != radiusSqr)
+        {
+            target.position = towerCenter + (target.position - towerCenter).normalized * playerRadius;
+        }
+    }
+
+    public static void SnapToGrid(Transform target, Tower center, float movementlayer)
+    {
+        var towerCenter = new Vector3(center.transform.position.x, target.position.y, center.transform.position.z);
+        var playerRadius = center.Radius + movementlayer;
+
+        var distanceSqr = (target.position - towerCenter).sqrMagnitude;
+        float radiusSqr = center.Radius * center.Radius;
+
+        Vector3 tangentVector = Quaternion.Euler(0, 0, 0) * (target.position - towerCenter);
+        target.rotation = Quaternion.LookRotation(tangentVector);
+
+        if (distanceSqr != radiusSqr)
+        {
+            target.position = towerCenter + (target.position - towerCenter).normalized * playerRadius;
+        }
+    }
 }
